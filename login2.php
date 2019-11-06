3d4e655bb026a9742a3cd381a8c8cd7aca12564e
MaxiArienzo <57308707+MaxiArienzo@users.noreply.github.com>
2019-11-06 20:51:10 -0300

    Add files via upload

---
+++ login2.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="estilos.css" rel="stylesheet" type="text/css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+	<br>
+	<br>
+	<body background="image/fondo.png">
+</head>
+<body>
+	<div class="text-center">
+      <img src="image/membrete.png" class="img-fluid" alt="Responsive image">
+	</div>
+	
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+<div class="fadeIn first">
+
+    </div>
+    <!-- Icon -->
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="Usuario">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="Contraseña">
+      <input type="submit" class="fadeIn fourth" value="Ingresar">
+    </form>
+
+  </div>
+</div>
+	</body>
+</html>
